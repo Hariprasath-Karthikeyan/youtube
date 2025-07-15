ce8623e42767bb023b18ac3ed2d3c5cc2480b896
Hariprasath-Karthikeyan <160593717+Hariprasath-Karthikeyan@users.noreply.github.com>
2025-07-15 15:01:15 +0530

    axiosinstancs.js

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://yourtube-hspf.onrender.com",
+  baseURL: "https://youtube-yx93.onrender.com",
 });
 
 export default axiosInstance;
